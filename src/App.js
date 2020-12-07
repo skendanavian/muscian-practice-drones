7029546a0bc48809f4a2fa60dffc7fbd7317eb98
skendanavian <s.nissen.bass@gmail.com>
2020-12-07 16:46:35 -0600

    fixed href

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <h1>Musician Practice Drones</h1>
       <ButtonList />
       <p>
-        <a target="_blank" href="http://www.sorennissen.com">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.sorennissen.com"
+        >
           Soren Nissen
         </a>{" "}
         &copy; 2020 - All Rights Reserved
